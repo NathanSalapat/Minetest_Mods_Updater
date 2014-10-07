074fae244863500622e14ad678bf109a605c759d
NathanSalapat <rendertracer@gmail.com>
2014-10-07 09:18:26 -0500

    V 1.1
    Added the correct code for pulling changes from GIT
    stripped some personal date replacing with generic info.

--- update.sh
+++ update.sh
@@ -6,16 +6,14 @@
 MODS=`ls`
 
 for i in ${MODS}; do
-     echo "$i"  #Prints the current directory, useful for debugging and logs
+     echo "$i"  #Prints the current directory, useful for debugging and logs, pointless right now.
      cd $i      #Moves into the current directory, VERY IMPORTANT
-     ls         #Runs the update call for GIT 'obviously not right now'
+     git pull   #Runs the update call for GIT
      cd -;      #Moves back up to the parent directory
 done            #Once all directories have cycled through this ends the loop
 
 #Now that all files are updated copy them to the main directory.
 
-#rsync ./home/nathan/Mintest_Git ./home/nathan/.minetest/mods
+#rsync GIT_LOCATION MINETEST_MOD_LOCATION
 
 #Rename the folders that need it. This must be filled out manually.
-
-
